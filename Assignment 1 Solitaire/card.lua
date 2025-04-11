07943145d15a7cc2e5f06a17e25165d5c937a61d
Kenshin <kechchao@ucsc.edu>
2025-04-11 00:15:49 -0700

    adding click and drag
    Added so if u left click on a card it will move.

---
+++ Assignment 1 Solitaire/card.lua
@@ -0,0 +1,36 @@
+
+require "vector"
+
+CardClass = {}
+
+function CardClass:new(xPos, yPos, width, height, suit, number)
+  local card = {}
+  local metatable = {__index = CardClass}
+  setmetatable(card, metatable)
+  card.selected = false
+  card.position = Vector(xPos, yPos)
+  card.size = Vector(width, height)
+  card.suit = suit
+  card.number = number
+  return card
+end
+
+function CardClass:isClicked(mx, my)
+  local x = self.position.x
+  local y = self.position.y
+  local w = self.size.x
+  local h = self.size.y
+
+  return mx >= x and mx <= x + w and my >= y and my <= y + h
+end
+
+function CardClass:draw()
+  if self.selected then
+    love.graphics.setColor(1,1,0)
+  else
+    love.graphics.setColor(1,1,1)
+  end
+  love.graphics.rectangle("fill",self.position.x,self.position.y, self.size.x, self.size.y) 
+  love.graphics.setColor(0,0,0)
+  love.graphics.print(self.suit .. " " .. tostring(self.number), self.position.x + 5, self.position.y + 5)
+end
--- Assignment 1 Solitaire/main.lua
+++ Assignment 1 Solitaire/main.lua
@@ -1,6 +1,61 @@
+io.stdout:setvbuf("no")
+
+offsetX = -25
+offsetY = -25
+
+
+require "card"
+
+--for i = 1, 52 do
+--  print(i)
+--end
+
+
 function love.load()
+  draggingCard = nil
   screenWidth = 640
   screenHeight = 480
   love.window.setMode(screenWidth, screenHeight)
   love.graphics.setBackgroundColor(0.2, 0.7, 0.2, 1)
+  
+  cardTable = {}
+  
+  table.insert(cardTable, CardClass:new(screenWidth/2, screenHeight/2, 50,100, "clove", 5))
+  
+  --print(cardTable)
+end
+
+function love.update(dt)
+--something
+  if draggingCard then
+    local x, y = love.mouse.getPosition()
+    draggingCard.position.x = x + offsetX
+    draggingCard.position.y = y + offsetY
+  end
+  
+end
+
+function love.draw()
+  for i, card in ipairs(cardTable) do
+    card:draw()
+  end
+end
+
+function love.mousepressed(x, y, button)
+  if button == 1 then 
+    for i, card in ipairs(cardTable) do
+      if card:isClicked(x, y) then
+        card.selected = true
+        draggingCard = card
+      end
+    end
+  end
+end
+
+function love.mousereleased(x,y, button)
+  if button == 1 and draggingCard then
+    print(draggingCard.suit .. draggingCard.number .. "was dropped")
+    draggingCard.selected = false
+    draggingCard = nil
+  end
 end
