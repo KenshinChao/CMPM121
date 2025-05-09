290743d3908eb1446f9d8f6915640c8cf91b7894
KenshinChao <kechchao@ucsc.edu>
2025-05-09 11:43:18 -0700

    5/9 lecture done day 15 demo

--- Day 15 Demo/main.lua
+++ Day 15 Demo/main.lua
@@ -9,9 +9,19 @@ GRID_WIDTH = 64
 GRID_HEIGHT = 36
 
 cellGrid = {}
+isSimulating = true
+simulationSpeed = 4 --(1,7)
 
 aliveColor = {1,0.8,1,1}
-deadcolor = {0, 0.3, 0,1}
+deadColor = {0, 0.3, 0,1}
+
+stepLengths = {
+  1, 0.8, 0.6,
+  0.4, 
+  0.2, 0.1, 0.05
+}
+
+stepTimer = 0
 
 require "vector" 
 
@@ -31,46 +41,148 @@ function love.load()
       table.insert(cellColumns, math.random() > 0.65)
     end
     table.insert(cellGrid, cellColumns)
-    end
+  end
     
 end
 
 function love.update()
+  if not isSimulating then
+    return  
+  end
   
+  if stepTimer < stepLengths[simulationSpeed] then
+    stepTimer= stepTimer + love.timer.getDelta()
+  else
+  simulationStep()
+  stepTimer = 0
 end
+end
+
 
 function love.draw()
   for x, col in ipairs(cellGrid) do
     for y, cell in ipairs(col) do
       love.graphics.setColor(cell and aliveColor or deadColor)
       love.graphics.rectangle("fill", (x-1) * CELL_SIZE, (y-1) * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-end
-end
+    end
+  end
 
 end
+
+function love.keypressed(key, scancode, isrepeat)
+  --perform the function
+  if controls[key] == nil then
+    print("No action associated to " .. key .. "!")
+    return
+  end
+  
+  controls[key]()
+  
+end
+
 
 function simulationStep()
   function getNeighborsAlive(x, y)
     local result = 0
-    for w = -1, 1 do 
-      for h = -1, 1 do 
-        local targetPos = Vector(x,y) + Vector(w,h)
+    for w = -1, 1 do
+      for h = -1, 1 do
+        local targetPos = Vector(x, y) + Vector(w, h)
+        targetPos = wrapGridPos(targetPos)
         local targetCell = cellGrid[targetPos.x][targetPos.y]
-        if targetCell then
+        if targetCell and Vector(x, y) ~= targetPos then
           result = result + 1
         end
-        
       end
     end
+    return result
+  end
+  function wrapGridPos(gridPos)
+    -- Horizontal Wrapping
+    if gridPos.x > GRID_WIDTH then
+      gridPos.x = 1
+    elseif gridPos.x < 1 then
+      gridPos.x = GRID_WIDTH
+    end
+    -- Vertical Wrapping
+    if gridPos.y > GRID_HEIGHT then
+      gridPos.y = 1
+    elseif gridPos.y < 1 then
+      gridPos.y = GRID_HEIGHT
+    end
+    return gridPos
   end
   --[[
-  1 2 3
-  4 5 6
-  7 8 9
+  1 4 6
+  2 X 7
+  3 5 8
   ]]--
+  local cellBuffer = {}
   for x, col in ipairs(cellGrid) do
-    for y, cell in ipairs(col) do 
-      local neighborsAlive = 
+    local cellColumns = {}
+    for y, cell in ipairs(col) do
+      local neighborsAlive = getNeighborsAlive(x, y)
+      local shouldBeAlive = true
+      if cell then
+        shouldBeAlive = (neighborsAlive == 2 or neighborsAlive == 3)
+        --cellGrid[x][y] = (neighborsAlive == 2 or neighborsAlive == 3)
+      else
+        shouldBeAlive = neighborsAlive == 3
+        --cellGrid[x][y] = neighborsAlive == 3
+      end
+      table.insert(cellColumns, shouldBeAlive)
     end
+    table.insert(cellBuffer, cellColumns)
   end
+  cellGrid = cellBuffer
 end
+
+function playOrPause()
+  isSimulating = not isSimulating
+  
+  
+end
+
+function increaseSimulationSpeed()
+  modifySimulationSpeed(1)
+end
+
+function decreaseSimulationSpeed()
+  modifySimulationSpeed(-1)
+end
+
+function modifySimulationSpeed(delta)
+  simulationSpeed = math.min(math.max(simulationSpeed + delta, 1), #stepLengths)
+  
+end
+
+function clearBoard()
+    for x, col in ipairs(cellGrid) do
+      for y, cell in ipairs(col) do
+        cellGrid[x][y] = false
+      end
+      
+    end
+    
+end
+
+
+controls = {
+  ["space"] = playOrPause, 
+  ["return"] = simulationStep,
+  ["a"] = decreaseSimulationSpeed,
+  ["d"] = increaseSimulationSpeed,
+  ["delete"] = clearBoard
+}
+
+function love.mousepressed(x, y, button, istouch)
+  if button ~= 1 then
+    return  
+  end
+  
+  toggleCell(math.floor(x/CELL_SIZE) + 1, math.floor(y/CELL_SIZE) + 1)
+end
+
+function toggleCell(x, y)
+  cellGrid[x][y] = not cellGrid[x][y]  
+end
+
